1f87098bb031fc1cb90a28cfd75fcae2f39a0dc9
Andrés Lucas <alucas@redpoints.com>
2019-06-08 18:12:25 +0200

    WIP

--- children-iframe-com/src/App.js
+++ children-iframe-com/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// addEventListener support for IE8
+const bindEvent = (element, eventName, eventHandler) => {
+  if (element.addEventListener) {
+    element.addEventListener(eventName, eventHandler, false);
+  } else if (element.attachEvent) {
+    element.attachEvent('on' + eventName, eventHandler);
+  }
+};
 
-export default App;
+export default class App extends React.Component {
+  componentDidMount() {
+    // Send a message to the parent
+    const sendMessage = function (msg) {
+      // Make sure you are sending a string, and to stringify JSON
+      window.parent.postMessage(msg, '*');
+    };
+    const results = document.getElementById('results');
+    const messageButton = document.getElementById('message_button');
+    // Listen to messages from parent window
+    bindEvent(window, 'message', function (e) {
+      results.innerHTML = e.data;
+    });
+    // Send random message data on every button click
+    bindEvent(messageButton, 'click', function (e) {
+      const random = Math.random();
+      sendMessage('' + random);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello there, i'm an iframe</h1>
+        <p>Send Message: <button id="message_button">Hi parent</button></p>
+        <p>Got Message:</p>
+        <div id="results"></div>
+      </div>
+    );
+  }
+}
--- parent-iframe-com/src/App.js
+++ parent-iframe-com/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// addEventListener support for IE8
+const bindEvent = (element, eventName, eventHandler) => {
+  if (element.addEventListener){
+    element.addEventListener(eventName, eventHandler, false);
+  } else if (element.attachEvent) {
+    element.attachEvent('on' + eventName, eventHandler);
+  }
 }
 
-export default App;
+export default class App extends React.Component {
+  componentDidMount() {
+    const iframeSource = 'http://localhost:3001/';
+    const iframeId = 'the_iframe';
+    if (!document.getElementById(iframeId)) {
+      // Create the iframe
+      const iframe = document.createElement('iframe');
+      iframe.setAttribute('src', iframeSource);
+      iframe.setAttribute('id', 'the_iframe');
+      iframe.style.width = 450 + 'px';
+      iframe.style.height = 200 + 'px';
+      iframe.style['text-align'] = 'center';
+      iframe.onload = () => {
+        const  messageButton = document.getElementById('message_button');
+        const results = document.getElementById('results');
+        // Send a message to the child iframe
+        const sendMessage = function(msg) {
+          // Make sure you are sending a string, and to stringify JSON
+          iframeEl.contentWindow.postMessage(msg, '*');
+        };
+        // Send random messge data on every button click
+        bindEvent(messageButton, 'click', function (e) {
+          const random = Math.random();
+          sendMessage('' + random);
+        });
+        // Listen to message from child window
+        bindEvent(window, 'message', function (e) {
+          results.innerHTML = e.data;
+        });
+      };
+      document.getElementById('iframe-wrapper').appendChild(iframe);
+      // Send a message to the child iframe
+      const iframeEl = document.getElementById(iframeId);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Parent Window</h1>
+        <p>Send Message: <button id="message_button">Hi there iframe</button></p>
+        <p>Got Message:</p>
+        <div id="results"></div>
+        <br/>
+        <div id="iframe-wrapper" />
+      </div>
+    );
+  }
+}
